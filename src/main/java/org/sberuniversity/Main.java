cf5818d15fb795535e5e27d831f35283570f4197
Mikhaylovskiy Anton <toni_mikhaylovskiy@mail.ru>
2024-11-21 08:48:08 +0300

    Delete deprected algorithm

--- src/main/java/org/sberuniversity/Main.java
+++ src/main/java/org/sberuniversity/Main.java
@@ -3,15 +3,12 @@ package org.sberuniversity;
 import org.sberuniversity.phonebook.Phonebook;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Set<String> city = new HashSet<>();
         HashMap<String, Integer> citycounter = new HashMap<>();
         String[] cities = {
                 "Москва", "Петербург", "Нью-Йорк", "Лондон", "Париж", "Токио", "Москва", "Берлин", "Париж", "Лондон", "Сидней", "Москва", "Токио",
@@ -26,7 +23,6 @@ public class Main {
 
         };
         for (String s : cities) {
-            city.add(s);
             citycounter.put(s, citycounter.getOrDefault(s, 0) + 1);
         }
         for (Map.Entry<String, Integer> entry : citycounter.entrySet()) {
